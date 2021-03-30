4996f8dcf89b520e6d7c9048c01b63f15ba41029
= <=>
2021-03-30 11:19:19 +0700

    Membuat model

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Barang as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; // Model Eloquent
 
 class Barang extends Model
 {
-    use HasFactory;
+    protected $table="barangs"; // Eloquent akan membuat model barang menyimpan record di tabel barangs
+    public $timestamps= false;
+    protected $primaryKey = 'id_barang'; // Memanggil isi DB Dengan primarykey
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    'id_barang',
+    'kode_barang',
+    'nama_barang',
+    'kategori_barang',
+    'harga',
+    'qty',
+    ];
 }
