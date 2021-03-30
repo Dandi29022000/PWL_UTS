a35bd34535097c6ab94accd32e1a33fec70de36f
= <=>
2021-03-30 11:17:48 +0700

    Membuat resource controller

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {	
+        $barangs = Barang::where([
+            ['nama_barang','!=',Null],
+            [function($query)use($request){
+                if (($term = $request->term)) {
+                    $query->orWhere('nama_barang','LIKE','%'.$term.'%')->get();
+                }
+            }]
+        ])
+        ->orderBy('id_barang','asc')
+        ->simplePaginate(5);
+        
+        return view('barangs.index' , compact('barangs'))
+        ->with('i',(request()->input('page',1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('barangs.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Melakukan validasi data
+        $request->validate([
+            'id_barang' => 'required',
+            'kode_barang' => 'required',
+            'nama_barang' => 'required',
+            'kategori_barang' => 'required',
+            'harga' => 'required',
+            'qty' => 'required',
+        ]);
+
+        // Fungsi eloquent untuk menambah data
+        Barang::create($request->all());
+
+        // Jika data berhasil ditambahkan, akan kembali ke halaman utama
+        return redirect()->route('barangs.index')
+            ->with('success', 'Barang Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id_barang)
+    {
+        // Menampilkan detail data dengan menemukan/berdasarkan id_barang
+        $Barang = Barang::find($id_barang);
+        return view('barangs.detail', compact('Barang'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id_barang)
+    {
+        // Menampilkan detail data dengan menemukan berdasarkan Nim Mahasiswa untuk diedit
+        $Barang = Barang::find($id_barang);
+        return view('barangs.edit', compact('Barang'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id_barang)
+    {
+        // Melakukan validasi data
+        $request->validate([
+            'id_barang' => 'required',
+            'kode_barang' => 'required',
+            'nama_barang' => 'required',
+            'kategori_barang' => 'required',
+            'harga' => 'required',
+            'qty' => 'required',
+        ]);
+
+        // Fungsi eloquent untuk mengupdate data inputan kita
+        Barang::find($id_barang)->update($request->all());
+
+        // Jika data berhasil diupdate, akan kembali ke halaman utama
+        return redirect()->route('barangs.index')
+            ->with('success', 'Barang Berhasil Diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id_barang)
+    {
+        // Fungsi eloquent untuk menghapus data
+        Barang::find($id_barang)->delete();
+        return redirect()->route('barangs.index')
+            -> with('success', 'Barang Berhasil Dihapus');
+    }
+}
