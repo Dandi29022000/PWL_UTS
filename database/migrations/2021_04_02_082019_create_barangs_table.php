0fc89a591f8a0a18adf1ebde6c592f428d7a83f3
Dandi29022000 <sembarangboleh55@gmail.com>
2021-04-02 15:38:30 +0700

    Migrate tabel barang fiks

---
+++ database/migrations/2021_04_02_082019_create_barangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id('id_barang');
+            $table->string('kode_barang', 10);
+            $table->string('nama_barang', 20);
+            $table->string('kategori_barang', 10);
+            $table->integer('harga');
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
